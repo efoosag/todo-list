a10fe6bf9ce2619281c8427b1f35ef5d0d5fdeec
efoosag <96612103+efoosag@users.noreply.github.com>
2022-06-04 23:00:20 +0100

    add fuctioality

--- src/index.js
+++ src/index.js
@@ -1,59 +1,29 @@
-import * as task from './status.js';
+import * as tk from './status.js';
 import './style.css';
 
-let list = [
-  { description: 'Set up a new project with webpack', isCompleted: false, index: 0 },
-  { description: 'Set up a new project with webpack', isCompleted: false, index: 1 },
-  { description: 'Create an index.js file', isCompleted: false, index: 2 },
-  { description: 'Write a function to iterate over the tasks array and populate an HTML', isCompleted: false, index: 3 },
-];
+const loadTask = document.querySelector('.todo-list');
+const form = document.querySelector('.todo-form');
 
-function todoList() {
-  if (window.localStorage.getItem('localTasks')) {
-    const localTasks = window.localStorage.getItem('localTasks');
-    list = JSON.parse(localTasks);
-  }
-  document.querySelector('.todo-list').innerHTML = '';
-  list.forEach((item) => {
-    const taskElement = document.createElement('li');
-    taskElement.classList.add('task');
-    if (item.isCompleted) {
-      taskElement.classList.add('completed');
-    }
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('task-check');
-    checkbox.addEventListener('click', () => {
-      task.status(item, list);
-      todoList();
-    });
-    checkbox.checked = item.isCompleted;
-    taskElement.appendChild(checkbox);
-    const taskText = document.createElement('input');
-    taskText.classList = 'task-text';
-    taskText.value = item.description;
-    taskText.addEventListener('change', () => {
-      if (taskText.value.length > 0) {
-        item.description = taskText.value;
-        task.saveLocal(list);
-      }
-    });
-    taskElement.appendChild(taskText);
-    const dragIcon = document.createElement('i');
-    dragIcon.classList = 'fas fa-ellipsis-v drag icon';
-    taskElement.appendChild(dragIcon);
-    taskElement.draggable = 'true';
-    document.querySelector('.todo-list').appendChild(taskElement);
+let taskIndex = 0;
+let taskDescription = '';
+let taskCompleted = '';
+
+const displayBook = () => {
+  const tasks = tk.getLocalTasks();
+  tasks.forEach((task) => {
+    taskDescription = task.desc;
+    taskIndex = task.id;
+    const li = tk.loadListElement(taskIndex, taskDescription);
+    console.log(li.innerHTML);
+    loadTask.append(li);
   });
-}
+};
 
-todoList();
-document.querySelector('#taskForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-  task.add(list);
-  todoList();
+form.addEventListener('submit', (e) => {
+  const addDesc = document.querySelector('.todo-input').value;
+  e.preventDefault();
+
+  tk.addLocalTask(addDesc);
 });
-document.querySelector('.clearer').addEventListener('click', () => {
-  task.removeDone(list);
-  todoList();
-});
+
+displayBook();
--- src/status.js
+++ src/status.js
@@ -1,3 +1,56 @@
+export function getLocalTasks() {
+  let tasks;
+  if (window.localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(window.localStorage.getItem('tasks'));
+  }
+  return tasks;
+}
+
+export function addLocalTask(task) {
+  const tasks = getLocalTasks();
+  const id = tasks.length + 1;
+  const desc = task;
+  const completed = false;
+  tasks.push({ id, desc, completed });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  window.location.reload();
+}
+
+export function removeLocalTask(id) {
+  const tasks = getLocalTasks();
+  tasks.forEach((task, index) => {
+    if (task.id === parseInt(id, 10)) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+export function loadListElement(taskIndex, taskDescription) {
+  const li = document.createElement('li');
+  li.classList.add(
+    'list-group-item',
+    'justify-content-between',
+    'd-flex',
+    'align-items-center',
+    // eslint-disable-next-line comma-dangle
+    'task'
+  );
+  li.innerHTML = `<div class="form-check">
+  <input class="checkbox" type="checkbox"/>
+  <input class="desc" type="text" value= "${taskDescription}"/>
+ 
+</div>
+
+<i class="remove fa fa-trash-o delete" id="${taskIndex}"></i>`;
+  return li;
+}
+
+// old code
+
 export function saveLocal(list) {
   window.localStorage.setItem('localTasks', JSON.stringify(list));
 }
@@ -12,7 +65,11 @@ export function status(elem, list) {
 }
 
 export function add(list) {
-  list.push({ description: document.querySelector('#newTask').value, isCompleted: false, index: list.length });
+  list.push({
+    description: document.querySelector('#newTask').value,
+    isCompleted: false,
+    index: list.length,
+  });
   document.querySelector('#newTask').value = '';
   saveLocal(list);
 }
